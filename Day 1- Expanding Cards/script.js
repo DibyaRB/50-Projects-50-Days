252f639ba578b687af1e92627f8f1679bfb6e3c8
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-30 13:26:10 +0530

    Updating JS

--- Day 1- Expanding Cards/script.js
+++ Day 1- Expanding Cards/script.js
@@ -1,1 +1,14 @@
-console.log('Starter');
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',(e)=>{
+            removeActiveClasses();
+            panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel,index)=>{
+        panel.classList.remove('active');
+    })
+}
