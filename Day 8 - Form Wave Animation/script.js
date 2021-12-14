083d4f4dcc7559fa27cc04a385139ed3884657bb
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-14 23:34:14 +0530

    Finishing Project

--- Day 8 - Form Wave Animation/script.js
+++ Day 8 - Form Wave Animation/script.js
@@ -1,1 +1,7 @@
-console.log('Starter'); 
+const labels= document.querySelectorAll('.form-control label');
+
+labels.forEach((label)=>{
+    label.innerHTML = label.innerText.split('').map((letter, index)=>{
+        return `<span style="transition-delay:${index * 30}ms">${letter}</span>`
+    }).join('');
+})
