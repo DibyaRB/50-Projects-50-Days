30b866ee7da15ea10f79c167e15ac480988e16e0
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-16 10:21:40 +0530

    Finishing

--- Day 10 - Dad Jokes/script.js
+++ Day 10 - Dad Jokes/script.js
@@ -1,1 +1,36 @@
 console.log('Starter'); 
+
+const jokeEl= document.getElementById('joke');
+const jokeBtn= document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+
+generateJoke();
+
+async function generateJoke(){
+
+    const config= {
+        headers:{
+            'Aceept': 'application/json'
+        }
+    };
+    const res = await fetch('https://cors-anywhere.herokuapp.com/https://icanhazdadjoke.com',config);
+
+    const resp= await res.json();
+
+    jokeEl.innerHTML = resp.joke;
+
+}
+
+// function generateJoke(){
+
+//     const config= {
+//         headers:{
+//             'Aceept': 'application/json'
+//         }
+//     };
+//     fetch('https://cors-anywhere.herokuapp.com/https://icanhazdadjoke.com',config).then(res=>res.json()).then((resp)=>{
+//         jokeEl.innerHTML = resp.joke;
+//     });
+// }
